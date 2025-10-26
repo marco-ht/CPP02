4d034f58d2d9c8f2d55a9c0ae3b1f21722af616c
marco-ht <marco-ht@outlook.it>
2025-10-26 02:55:37 +0200

    01

--- ex01/Fixed.cpp
+++ ex01/Fixed.cpp
@@ -6,7 +6,7 @@
 /*   By: mpierant <marvin@student.42roma.it>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/25 22:55:40 by mpierant          #+#    #+#             */
-/*   Updated: 2025/10/26 02:53:04 by mpierant         ###   ########.fr       */
+/*   Updated: 2025/10/26 02:49:14 by mpierant         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,11 +47,36 @@ int Fixed::getRawBits( void ) const
 
 void    Fixed::setRawBits( int const raw )
 {
+    std::cout << "setRawBits member function called" << std::endl;
     fp = raw;
 }
 
+Fixed::Fixed(const int i)
+{
+    std::cout << "Int constructor called" << std::endl;
+    fp = i * (1 << P);
+}
+
+Fixed::Fixed(const float f)
+{
+    std::cout << "Float constructor called" << std::endl;
+    fp = (int)(f * (1 << P) + 0.5f);
+}
+
+float Fixed::toFloat( void ) const
+{
+    return (((float)(fp)) / (1 << P));
+}
+
+int Fixed::toInt( void ) const
+{
+    return (fp / (1 << P));
+}
+
+std::ostream& operator<<(std::ostream& os, const Fixed& obj)
+{
+    os << obj.toFloat(); 
+    return os;
+}
 
 
-
-//In Linux, il simbolo tilde (\(~\)) può essere digitato
-// con la combinazione di tasti Alt Gr + ì
--- ex01/Fixed.hpp
+++ ex01/Fixed.hpp
@@ -6,12 +6,14 @@
 /*   By: mpierant <marvin@student.42roma.it>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/25 06:32:05 by mpierant          #+#    #+#             */
-/*   Updated: 2025/10/26 02:51:33 by mpierant         ###   ########.fr       */
+/*   Updated: 2025/10/26 02:20:29 by mpierant         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FIXED_HPP
 # define FIXED_HPP
+
+#include <iostream>
 
 class Fixed
 {
@@ -22,10 +24,17 @@ class Fixed
     public:
     Fixed();
     Fixed(const Fixed &obj);
+    Fixed(const int i);
+    Fixed(const float f);
     Fixed& operator=(const Fixed& other);
     ~Fixed();
+    
     int getRawBits( void ) const;
     void    setRawBits( int const raw );
+    float toFloat( void ) const;
+    int toInt( void ) const;
 };
+
+std::ostream& operator<<(std::ostream& os, const Fixed& obj);
 
 #endif
